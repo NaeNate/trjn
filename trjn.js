a02a1c23f95452d5741ebb235d7535412745a699
Nate Davis <nate+GIT@qstreet.org>
2022-07-22 19:55:29 -0400

    Update to use dynamic PORT

--- trjn.js
+++ trjn.js
@@ -37,7 +37,7 @@ const server = http.createServer((req, res) => {
   })
 })
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 server.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`)
